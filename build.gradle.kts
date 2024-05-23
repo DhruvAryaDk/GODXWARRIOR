44145fc4dad934dbd383a16b2bbf6a674559b587
DhruvAryaDk <111742621+DhruvAryaDk@users.noreply.github.com>
2024-05-24 02:21:08 +0530

    Update build.gradle.kts
    Signed-off-by: DhruvAryaDk <111742621+DhruvAryaDk@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,5 @@
 id("com.google.gms.google-services") version "4.4.1" apply false
 id("com.android.application")
 id("com.google.gms.google-services")
+implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+implementation("com.google.firebase:firebase-analytics")
