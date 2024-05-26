3e4a1cb660cbf9ea4417be168740b17c65d60c72
DhruvAryaDk <111742621+DhruvAryaDk@users.noreply.github.com>
2024-05-26 11:15:40 +0530

    Delete build.gradle.kts
    Signed-off-by: DhruvAryaDk <111742621+DhruvAryaDk@users.noreply.github.com>

--- build.gradle.kts
+++
@@ -1,6 +0,0 @@
-<project>/build.gradle.kts
-id("com.google.gms.google-services") version "4.4.1" apply false
-id("com.android.application")
-id("com.google.gms.google-services")
-implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
-implementation("com.google.firebase:firebase-analytics")
